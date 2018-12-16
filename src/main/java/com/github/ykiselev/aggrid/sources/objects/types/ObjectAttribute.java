3162f6316c947bc950f1614412c0c8fec3d3d9ed
Uze <Uze@yandex.ru>
2018-12-16 20:55:35 +0300

    Aggregation unit test + obj attribute

---
+++ src/main/java/com/github/ykiselev/aggrid/sources/objects/types/ObjectAttribute.java
@@ -0,0 +1,41 @@
+package com.github.ykiselev.aggrid.sources.objects.types;
+
+import java.util.function.Function;
+import java.util.function.ToDoubleFunction;
+import java.util.function.ToIntFunction;
+import java.util.function.ToLongFunction;
+
+import static java.util.Objects.requireNonNull;
+
+/**
+ * @author Yuriy Kiselev (uze@yandex.ru).
+ */
+public final class ObjectAttribute<V> extends AbstractAttribute<V> {
+
+    private final Function<V, ?> getter;
+
+    public ObjectAttribute(String name, Class<?> type, Function<V, ?> getter) {
+        super(name, type);
+        this.getter = requireNonNull(getter);
+    }
+
+    @Override
+    public ToIntFunction<V> getIntGetter() {
+        return v -> (Integer) getter.apply(v);
+    }
+
+    @Override
+    public ToLongFunction<V> getLongGetter() {
+        return v -> (Long) getter.apply(v);
+    }
+
+    @Override
+    public ToDoubleFunction<V> getDoubleGetter() {
+        return v -> (Double) getter.apply(v);
+    }
+
+    @Override
+    public Function<V, ?> getObjectGetter() {
+        return getter;
+    }
+}
